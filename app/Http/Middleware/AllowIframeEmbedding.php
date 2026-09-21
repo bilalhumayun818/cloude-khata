2c8072aae3b8a18bd0cb92f56371eedba49d3997
SafiullahZafar <safullahzafar@gmail.com>
2026-09-20 23:41:27 -0700

    making demo correct

--- app/Http/Middleware/AllowIframeEmbedding.php
+++ app/Http/Middleware/AllowIframeEmbedding.php
@@ -3,6 +3,9 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+use App\Utils\BusinessUtil;
 
 class AllowIframeEmbedding
 {
@@ -15,6 +18,42 @@ class AllowIframeEmbedding
      */
     public function handle($request, Closure $next)
     {
+        // Auto-login demo user for iframe previews if unauthenticated
+        if (! Auth::check()) {
+            $demoUser = User::first();
+            if ($demoUser) {
+                Auth::login($demoUser);
+
+                $business_util = new BusinessUtil();
+                $business = \App\Business::find($demoUser->business_id);
+                if ($business) {
+                    $currency = $business->currency;
+                    $currency_data = [
+                        'id' => $currency->id ?? 1,
+                        'code' => $currency->code ?? 'USD',
+                        'symbol' => $currency->symbol ?? '$',
+                        'thousand_separator' => $currency->thousand_separator ?? ',',
+                        'decimal_separator' => $currency->decimal_separator ?? '.',
+                    ];
+
+                    session([
+                        'user' => [
+                            'id' => $demoUser->id,
+                            'surname' => $demoUser->surname,
+                            'first_name' => $demoUser->first_name,
+                            'last_name' => $demoUser->last_name,
+                            'email' => $demoUser->email,
+                            'business_id' => $demoUser->business_id,
+                            'language' => $demoUser->language,
+                        ],
+                        'business' => $business,
+                        'currency' => $currency_data,
+                        'financial_year' => $business_util->getCurrentFinancialYear($business->id),
+                    ]);
+                }
+            }
+        }
+
         $response = $next($request);
 
         if (method_exists($response, 'header')) {
